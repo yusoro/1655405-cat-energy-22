9ce5b322abca8612b37d2d7011bab496cf63e508
Юлия Сорокина <riajulia@yandex.ru>
2021-04-25 13:04:54 +0300

    Декоративные элементы страницы формы

--- source/js/slider-range.js
+++ source/js/slider-range.js
@@ -1,23 +1,16 @@
 if (document.documentElement.clientWidth > 767) {
   document.querySelector(".slider__catlook-after").classList.remove("hidden");
-  document.querySelectorAll(".slider").forEach(function(slider) {
+  document.querySelectorAll(".slider__photos").forEach(function(slider) {
     // Создаем дополнительный блок div и
     // оборачиваем его вокруг первого изображения
     let div = document.createElement("div");
-    let img = slider.querySelector("img");
+    let img = slider.querySelector("picture");
     slider.insertBefore(div, img);
     div.append(img);
     // Создаем ползунок
-    let range = document.createElement("input");
-    range.type = "range";
-    range.classList.add("slider__range");
+    let range = document.querySelector(".slider__range");
     range.oninput = function() {
     div.style.width = this.value + "%";
-    };
-    let sliderControls = document.querySelector(".slider__controls");
-    sliderControls.append(range);
+    }
+  })};
 
-    let before = document.querySelector(".slider__after");
-    sliderControls.insertBefore(range, before);
-    });
-  }
