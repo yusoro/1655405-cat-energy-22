a080e5aa9edfc083a7e31f0b58be0a684f3e55d6
Юлия Сорокина <riajulia@yandex.ru>
2021-05-18 22:23:22 +0300

    Автоматизация сборки личного проекта

--- source/js/slider-range.js
+++
@@ -1,5 +0,0 @@
-    let sliderBefore = document.querySelector(".slider__before");
-    let range = document.querySelector(".slider__range");
-    range.oninput = function() {
-    sliderBefore.style.width = this.value + "%";
-    }
